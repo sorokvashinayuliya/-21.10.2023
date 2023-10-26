f9567dba94642ed456a8e625998cb2887c1a27e9
79176 <79176@LAPTOP-MN0I58DS>
2023-10-26 23:36:56 +0400

    Добавьте файлы проекта.

---
+++ ДЗ 21.10.2023/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ДЗ_21._10._2023
+{
+    enum WorkingDepartments
+    {
+        системщики,
+        разработчики,
+        начальство,
+    }
+    class Employee
+    {
+        
+        private string employeeName;
+        private string jobTitle;
+        private WorkingDepartments workDepartment;
+        private List<Employee> superiors = new List<Employee>();
+        
+        public string EmployeeName
+        {
+            get
+            {
+                return employeeName;
+            }
+        }
+        public string JobTitle
+        {
+            get
+            {
+                return jobTitle;
+            }
+        }
+        public WorkingDepartments WorkDepartment
+        {
+            get
+            {
+                return workDepartment;
+            }
+        }
+        public List<Employee> Superiors
+        {
+            get
+            {
+                return superiors;
+            }
+        }
+       
+        class Task
+        {
+            
+            private string taskContent;
+            private WorkingDepartments taskAddress;
+           
+            public string TaskContent
+            {
+                get
+                {
+                    return taskContent;
+                }
+            }
+            public WorkingDepartments TaskAddress
+            {
+                get
+                {
+                    return taskAddress;
+                }
+            }
+           
+            public Task(string taskContent, WorkingDepartments taskAddress)
+            {
+                this.taskContent = taskContent;
+                this.taskAddress = taskAddress;
+            }
+         
+        }
+       
+        public void AssignmentOfTask(Task task, Employee employee)
+        {
+            Console.WriteLine($"От {employee.EmployeeName} ({employee.JobTitle}) дается задача\n{task.TaskContent} работнику {employeeName} ({jobTitle})");
+
+            if (superiors.Contains(employee) && (workDepartment == task.TaskAddress))
+            {
+                Console.WriteLine($"{employeeName} ({jobTitle}) берет задачу\n");
+            }
+            else
+            {
+                Console.WriteLine($"{employeeName} ({jobTitle}) не берет задачу\n");
+            }
+        }
+        
+        public Employee(string employeeName, string jobTitle, WorkingDepartments workDepartment, params Employee[] superiors)
+        {
+            this.employeeName = employeeName;
+            this.jobTitle = jobTitle;
+            this.workDepartment = workDepartment;
+            this.superiors.AddRange(superiors);
+        }
+      
+        internal class Program
+        {
+            static void Main()
+            {
+                
+                
+                Console.WriteLine("ПРОГРАММА СОЗДАЕТ ИЕРАРХИЮ СОТРУДНИКОВ, ДАЕТ ИМ ЗАДАЧИ И ВЫВОДИТ НА ЭКРАН, БЕРУТ ОНИ ЕЕ ИЛИ НЕТ\n");
+                
+                Employee Semyon = new Employee("Семен", "Генеральный директор", WorkingDepartments.начальство);
+
+                Employee Rashid = new Employee("Рашид", "Финансовый директор", WorkingDepartments.начальство, Semyon);
+                Employee Lucas = new Employee("Лукас", "Начальник бухгалтерии", WorkingDepartments.начальство, Rashid);
+
+                Employee OIlham = new Employee("О Ильхам", "Директор по автоматизации", WorkingDepartments.начальство, Semyon);
+                Employee Orkady = new Employee("Оркадий", "Начальник отдела информационных технологий", WorkingDepartments.начальство, OIlham);
+                Employee Volodya = new Employee("Володя", "Зам. начальника отдела информационных технологий", WorkingDepartments.начальство, Orkady);
+
+                Employee Ilshat = new Employee("Ильшат", "Начальник отдела системщиков", WorkingDepartments.начальство, Orkady, Volodya);
+                Employee Ivanych = new Employee("Иваныч", "Зам. начальника отдела системщиков", WorkingDepartments.начальство, Ilshat);
+
+                Employee Ilya = new Employee("Илья", "Работник отдела системщиков", WorkingDepartments.системщики, Ivanych);
+                Employee Vitya = new Employee("Витя", "Работник отдела системщиков", WorkingDepartments.системщики, Ivanych);
+                Employee Zhenya = new Employee("Женя", "Работник отдела системщиков", WorkingDepartments.системщики, Ivanych);
+
+                Employee Sergey = new Employee("Сергей", "Начальник отдела разработчиков", WorkingDepartments.начальство, Orkady, Volodya);
+                Employee Laysan = new Employee("Ляйсан", "Зам. начальника отдела разработчиков", WorkingDepartments.начальство, Sergey);
+
+                Employee Marat = new Employee("Марат", "Работник отдела разработчиков", WorkingDepartments.разработчики, Laysan);
+                Employee Dina = new Employee("Дина", "Работник отдела разработчиков", WorkingDepartments.разработчики, Laysan);
+                Employee Ildar = new Employee("Ильдар", "Работник отдела разработчиков", WorkingDepartments.разработчики, Laysan);
+                Employee Anton = new Employee("Антон", "Работник отдела разработчиков", WorkingDepartments.разработчики, Laysan);
+                
+                Task task1 = new Task("Расчитать прибыль компании", WorkingDepartments.начальство);
+                Task task2 = new Task("Разработать приложение", WorkingDepartments.разработчики);
+                Task task3 = new Task("Настроить работу корпоративной сети", WorkingDepartments.системщики);
+                Task task4 = new Task("Провести собрание с сотрудниками", WorkingDepartments.начальство);
+                
+
+                Ilya.AssignmentOfTask(task3, Ivanych);
+                Vitya.AssignmentOfTask(task3, Lucas);
+                Zhenya.AssignmentOfTask(task1, Ivanych);
+
+                Marat.AssignmentOfTask(task2, Laysan);
+                Dina.AssignmentOfTask(task3, Laysan);
+                Ildar.AssignmentOfTask(task2, Sergey);
+                Anton.AssignmentOfTask(task4, Marat);
+
+                Sergey.AssignmentOfTask(task4, Orkady);
+                Sergey.AssignmentOfTask(task4, Volodya);
+            }
+        }
+    }
+}
+
---
+++ Тумаков/Program.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Тумаков
+{
+    internal class Program
+    {
+        public enum AccountType
+        {
+            Текущий_счет,
+            Сберегательный_счет
+        }
+        class BankAccount
+        {
+            private static int numberOfBankAccounts;
+            private int accountNumber;
+            private decimal accountBalance;
+            private AccountType bankAccountType;
+            public int AccountNumber
+            {
+                get
+                {
+                    return accountNumber;
+                }
+            }
+            public decimal AccountBalance
+            {
+                get
+                {
+                    return accountBalance;
+                }
+            }
+            public AccountType BankAccountType
+            {
+                get
+                {
+                    return bankAccountType;
+                }
+            }
+
+            public static int NumberOfBankAccounts { get => numberOfBankAccounts; set => numberOfBankAccounts = value; }
+            private void ChangeNumberOfBankAccounts()
+            {
+                NumberOfBankAccounts++;
+            }
+            public bool WithdrawMoneyFromAccount(decimal withdrawalAmount)
+            {
+                if ((accountBalance - withdrawalAmount > 0) && (withdrawalAmount > 0))
+                {
+                    accountBalance -= withdrawalAmount;
+                    return true;
+                }
+
+                return false;
+            }
+            public bool PutMoneyIntoAccount(decimal depositedAmoun)
+            {
+                if (depositedAmoun > 0)
+                {
+                    accountBalance += depositedAmoun;
+                    return true;
+                }
+
+                return false;
+            }
+            public bool TransferringMoney(BankAccount withdrawalAccount, decimal withdrawalAmount)
+            {
+                if ((withdrawalAmount > 0) && (withdrawalAccount.AccountBalance - withdrawalAmount > 0))
+                {
+                    accountBalance += withdrawalAmount;
+                    withdrawalAccount.accountBalance -= withdrawalAmount;
+                    return true;
+                }
+
+                return false;
+            }
+            public BankAccount(AccountType bankAccountType)
+            {
+                accountNumber = NumberOfBankAccounts;
+                accountBalance = 0;
+                this.bankAccountType = bankAccountType;
+                ChangeNumberOfBankAccounts();
+            }
+        }
+        class Song
+        {
+            private string songName;
+            private string songAuthor;
+            private Song previousSong;
+            public string SongName
+            {
+                get
+                {
+                    return songName;
+                }
+            }
+            public string SongAuthor
+            {
+                get
+                {
+                    return songAuthor;
+                }
+            }
+            public Song PreviousSong
+            {
+                get
+                {
+                    return previousSong;
+                }
+            }
+            public string Title
+            {
+                get
+                {
+                    return songName + " " + songAuthor;
+                }
+            }
+            public override bool Equals(object transmittedSong)
+            {
+                Song song = transmittedSong as Song;
+
+                if ((song != null) && (song.SongName == songName) && (song.SongAuthor == songAuthor))
+                {
+                    return true;
+                }
+
+                return false;
+            }
+            public override int GetHashCode()
+            {
+                return base.GetHashCode();
+            }
+            public Song(string songName, string songAuthor, Song previousSong)
+            {
+                this.songName = songName;
+                this.songAuthor = songAuthor;
+                this.previousSong = previousSong;
+            }
+            public Song(string songName, string songAuthor)
+            {
+                this.songName = songName;
+                this.songAuthor = songAuthor;
+                previousSong = null;
+            }
+        }
+        static string ReversesTheOrderOfCharactersInString(string sourceString)
+        {
+            char[] stringCharacterArray = sourceString.ToCharArray();
+
+            Array.Reverse(stringCharacterArray);
+
+            return String.Concat(stringCharacterArray);
+        }
+        static bool ChecksObjectImplementsIFormattableUsingIs(object checkedObject)
+        {
+            if (checkedObject is IFormattable)
+            {
+                return true;
+            }
+
+            return false;
+        }
+        static bool ChecksObjectImplementsIFormattableUsingAs(object checkedObject)
+        {
+            if (checkedObject as IFormattable == null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+        static bool ExtractingEmailFromDataString(ref string dataString)
+        {
+            string[] data = dataString.Split(new char[] { '#' }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (data.Length == 2)
+            {
+                dataString = data[1];
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        class ClassImplementingIFormattables : IFormattable
+        {
+            private int firstValue = 10;
+
+            public override string ToString()
+            {
+                return ToString("G", NumberFormatInfo.CurrentInfo);
+            }
+
+            public string ToString(string format)
+            {
+                return ToString(format, NumberFormatInfo.CurrentInfo);
+            }
+
+            public string ToString(string format, IFormatProvider provider)
+            {
+                return firstValue.ToString("G", provider);
+            }
+        }
+
+        static void Main()
+        {
+            bool tasksEnd = false;
+            string taskNumber;
+            Console.Clear();
+            Console.WriteLine("{0, 112}", "УПРАЖНЕНИЕ 8.1. ПРОГРАММА ИЗ УПРАЖНЕНИЯ 7.3, НО ДОБАВЛЕН МЕТОД ПЕРЕВОДА ДЕНЕГ С ОДНОГО СЧЕТА НА ДРУГОЙ\n");
+
+            BankAccount firstBankAccount = new BankAccount(AccountType.Текущий_счет);
+            BankAccount secondBankAccount = new BankAccount(AccountType.Текущий_счет);
+            bool putMoneyResult, transferringMoneyResult;
+
+            putMoneyResult = firstBankAccount.PutMoneyIntoAccount(500000.55M);
+            putMoneyResult &= secondBankAccount.PutMoneyIntoAccount(100000.87M);
+
+            if (putMoneyResult)
+            {
+                Console.WriteLine($"{firstBankAccount.BankAccountType} №{firstBankAccount.AccountNumber:D4}, баланс: {firstBankAccount.AccountBalance} рублей\t" +
+                                  $"{secondBankAccount.BankAccountType} №{secondBankAccount.AccountNumber:D4}, баланс: {secondBankAccount.AccountBalance} рублей");
+
+                transferringMoneyResult = firstBankAccount.TransferringMoney(secondBankAccount, 50000.87M);
+
+                if (transferringMoneyResult)
+                {
+                    Console.WriteLine($"{firstBankAccount.BankAccountType} №{firstBankAccount.AccountNumber:D4}, баланс: {firstBankAccount.AccountBalance} рублей\t" +
+                                      $"{secondBankAccount.BankAccountType} №{secondBankAccount.AccountNumber:D4}, баланс: {secondBankAccount.AccountBalance} рублей");
+                }
+                else
+                {
+                    Console.WriteLine("На банковском счету недостаточно средств или вы неверно ввели сумму!");
+                }
+
+                transferringMoneyResult = secondBankAccount.TransferringMoney(firstBankAccount, 500000);
+
+                if (transferringMoneyResult)
+                {
+                    Console.WriteLine($"{firstBankAccount.BankAccountType} №{firstBankAccount.AccountNumber:D4}, баланс: {firstBankAccount.AccountBalance} рублей\t" +
+                                      $"{secondBankAccount.BankAccountType} №{secondBankAccount.AccountNumber:D4}, баланс: {secondBankAccount.AccountBalance} рублей");
+                }
+                else
+                {
+                    Console.WriteLine("На банковском счету недостаточно средств или вы неверно ввели сумму!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Вы неверно ввели сумму!");
+            }
+
+
+
+            Console.WriteLine("{0, 111}", "УПРАЖНЕНИЕ 8.2. ПРОГРАММА ПОЛУЧАЕТ СТРОКУ И ВОЗВРАЩАЕТ НОВУЮ, СИМВОЛЫ В КОТОРОЙ ИДУТ В ОБРАТНОМ ПОРЯДКЕ\n");
+
+            string userString, newString;
+
+            Console.Write("Введите строку: ");
+            userString = Console.ReadLine();
+
+            newString = ReversesTheOrderOfCharactersInString(userString);
+            Console.Write($"Из строки {userString} получилась строка: {newString}");
+
+
+            Console.WriteLine("{0, 111}", "УПРАЖНЕНИЕ 8.4. ПРОГРАММА ПРОВЕРЯЕТ РЕАЛИЗУЕТ ЛИ ПЕРЕДАВАЕМЫЙ ОБЪЕКТ ИНТРЕФЕЙС System.IFormattable\n");
+
+            ClassImplementingIFormattables firstObject = new ClassImplementingIFormattables();
+            BankAccount secondObject = new BankAccount(AccountType.Текущий_счет);
+
+            if (ChecksObjectImplementsIFormattableUsingIs(firstObject))
+            {
+                Console.WriteLine("Объект реализует интерфейс System.IFormattable");
+            }
+            else
+            {
+                Console.WriteLine("Объект не реализует интерфейс System.IFormattable");
+            }
+
+            if (ChecksObjectImplementsIFormattableUsingIs(secondObject))
+            {
+                Console.WriteLine("Объект реализует интерфейс System.IFormattable");
+            }
+            else
+            {
+                Console.WriteLine("Объект не реализует интерфейс System.IFormattable");
+            }
+
+            if (ChecksObjectImplementsIFormattableUsingAs(firstObject))
+            {
+                Console.WriteLine("Объект реализует интерфейс System.IFormattable");
+            }
+            else
+            {
+                Console.WriteLine("Объект не реализует интерфейс System.IFormattable");
+            }
+
+            if (ChecksObjectImplementsIFormattableUsingAs(secondObject))
+            {
+                Console.WriteLine("Объект реализует интерфейс System.IFormattable");
+            }
+            else
+            {
+                Console.WriteLine("Объект не реализует интерфейс System.IFormattable");
+            }
+
+
+
+            Console.WriteLine("{0, 113}", "ДОМАШНЕЕ ЗАДАНИЕ 8.1. ПРОГРАММА ВЫДЕЛЯЕТ ИЗ ВХОДНОГО ФАЙЛА С ДАННЫМИ E-MAIL И ЗАПИСЫВАЕТ ЕГО В НОВОМ ФАЙЛЕ\n");
+
+            bool extractingResult;
+            string[] dataFile = File.ReadAllLines("ProgramFiles/DataFile.txt");
+
+            for (int i = 0; i < dataFile.Length; i++)
+            {
+                string dataString = dataFile[i];
+                extractingResult = ExtractingEmailFromDataString(ref dataString);
+                if (extractingResult)
+                {
+                    File.AppendAllText("ProgramFiles/EmailFile.txt", dataString + Environment.NewLine);
+                }
+                else
+                {
+                    Console.WriteLine("Входные данные заполнены с ошибкой. Проверьте входной файл!");
+                    break;
+                }
+            }
+            Console.WriteLine("{0, 110}", "ДОМАШНЕЕ ЗАДАНИЕ 8.2. ПРОГРАММА СОЗДАЕТ СПИСОК ПЕСЕН, ВЫВОДИТ ИХ НА ЭКРАН И СРАВНИВАЕТ ДВЕ ИЗ НИХ\n");
+
+            Song firstSong = new Song("Я Русский", "Shaman");
+            Song secondSong = new Song("Моя Россия", "Shaman", firstSong);
+            Song thirdSong = new Song("Вороны мои", "Shaman", secondSong);
+            Song fourthSong = new Song("Гимн России", "Shaman", thirdSong);
+
+            List<Song> songList = new List<Song> { firstSong, secondSong, thirdSong, fourthSong };
+
+            foreach (Song song in songList)
+            {
+                Console.WriteLine(song.Title);
+            }
+
+            if (firstSong.Equals(secondSong))
+            {
+                Console.WriteLine("\nПесни равны!");
+            }
+            else
+            {
+                Console.WriteLine("\nПесни неравны!");
+            }
+
+        }
+           
+    }
+}
